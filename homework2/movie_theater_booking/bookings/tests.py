5a9f43a42fdb7569a1324a10a86cd147dc7a91e0
BenDaHen <bennyhen121@gmail.com>
2025-10-06 21:35:42 +0000

    Revert "Finished tests, flushed database in preparation for render"
    This reverts commit 0b20957aea49f07a12a4ab4cdc3560045f72194d.

--- homework2/movie_theater_booking/bookings/tests.py
+++ homework2/movie_theater_booking/bookings/tests.py
@@ -24,29 +24,8 @@ class MovieModelTest(TestCase):
         self.assertEqual(self.movie.movie_description, "This is a test movie")
         self.assertEqual(self.movie.movie_release_date, date(2025, 1, 11))
         self.assertEqual(self.movie.movie_duration, timedelta(hours=2, minutes=30))
-        self.assertEqual(Movie.objects.count(), 1)
 
 
-    #Test that a movie is correctly updated
-    def test_movie_update(self):
-        self.movie.movie_title = "New Title"
-        self.movie.movie_description = "New Description"
-        self.movie.movie_release_date = date(2025, 2, 22)
-        self.movie.movie_duration = timedelta(hours=1, minutes=20)
-        self.movie.save()
-
-        self.assertEqual(self.movie.movie_title, "New Title")
-        self.assertEqual(self.movie.movie_description, "New Description")
-        self.assertEqual(self.movie.movie_release_date, date(2025, 2, 22))
-        self.assertEqual(self.movie.movie_duration, timedelta(hours=1, minutes=20))
-
-    #Test that a movie is correctly deleted
-    def test_movie_delete(self):
-        self.movie.delete()
-
-        self.assertEqual(Movie.objects.count(), 0)
-
-#Seat Tests
 class SeatModelTest(TestCase):
     """Tests for the seat model"""
 
@@ -77,72 +56,3 @@ class SeatModelTest(TestCase):
         self.seat.delete()
 
         self.assertEqual(Seat.objects.count(), 0)
-
-#Booking Model
-class BookingModelTest(TestCase):
-    """Tests for the booking model"""
-
-    #Sets up a booking to be the same for all tests
-    def setUp(self):
-         #Set up a Movie and Seat to be used with the booking
-        self.movie = Movie.objects.create(
-            movie_title = "Test Movie Title",
-            movie_description = "This is a test movie",
-            movie_release_date = date(2025, 1, 11),
-            movie_duration=timedelta(hours=2, minutes=30)
-        )
-
-        self.seat = Seat.objects.create(
-            seat_number = 10,
-            booking_status = True
-        )
-
-        #Create the booking using the movie and seat
-        self.booking = Booking.objects.create(
-            booked_movie = self.movie,
-            seat = self.seat,
-            booked_date = date(2025, 5, 25),
-            booked_user = "User"
-        )
-
-        self.movie2 = Movie.objects.create(
-            movie_title = "Test Movie Title 2",
-            movie_description = "This is a test movie 2",
-            movie_release_date = date(2025, 3, 29),
-            movie_duration=timedelta(hours=1, minutes=30)
-        )
-
-        self.seat2 = Seat.objects.create(
-            seat_number = 20,
-            booking_status = False
-        )
-
-        #Set up an additional movie and seat for editing
-
-
-    #Test that a booking is correctly created
-    def test_booking_creation(self):
-        self.assertEqual(self.booking.booked_movie, self.movie)
-        self.assertEqual(self.booking.seat, self.seat)
-        self.assertEqual(self.booking.booked_date, date(2025, 5, 25))
-        self.assertEqual(self.booking.booked_user, "User")
-        self.assertEqual(Booking.objects.count(), 1)
-
-    #Test that a booking is correctly edited
-    def test_booking_edit(self):
-        self.booking.booked_movie = self.movie2
-        self.booking.seat = self.seat2
-        self.booking.booked_date = date(2025, 4, 24)
-        self.booking.booked_user = "User 2"
-        self.booking.save()
-
-        self.assertEqual(self.booking.booked_movie, self.movie2)
-        self.assertEqual(self.booking.seat, self.seat2)
-        self.assertEqual(self.booking.booked_date, date(2025, 4, 24))
-        self.assertEqual(self.booking.booked_user, "User 2")
-
-    #Test that a booking is correctly deleted
-    def test_booking_delete(self):
-        self.booking.delete()
-
-        self.assertEqual(Booking.objects.count(), 0)
